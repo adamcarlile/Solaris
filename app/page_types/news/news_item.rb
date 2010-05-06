d1d8b74c586ec089f61633d3532924741015a7d3
Adam Carlile <adam@benchmedia.co.uk>
2010-05-07 00:46:47 +0100

    Added fixes

--- app/page_types/news/news_item.rb
+++ app/page_types/news/news_item.rb
@@ -4,6 +4,7 @@ class NewsItem < Page
   self.page_type_package = 'news'
   self.can_have_children = false
   self.can_have_comments = false
+  self.show_in_nav       = false
   
   def self.latest(number = 10)
     published.most_recent_first.limit(number)
